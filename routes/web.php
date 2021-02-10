aa538530a7c0c4841cbe1e324e728eae371e7a95
Mirko Skopljak <45762923+mirko93@users.noreply.github.com>
2021-02-10 09:18:00 +0100

    adds author resource

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\BooksController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
+use App\Http\Controllers\AuthorsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/books', [BooksController::class, 'store']);
 Route::patch('/books/{book}', [BooksController::class, 'update']);
+Route::delete('/books/{book}', [BooksController::class, 'destroy']);
+
+Route::post('/author', [AuthorsController::class, 'store']);
