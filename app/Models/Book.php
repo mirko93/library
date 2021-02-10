86d10086026e449d3612e7f75c5b836b7988efdc
Mirko Skopljak <45762923+mirko93@users.noreply.github.com>
2021-02-10 12:36:05 +0100

    adds relationship and checkin, checkout new line in database reservations

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use App\Models\Author;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
@@ -17,10 +19,41 @@ class Book extends Model
         return '/books/' . $this->id;
     }
 
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        // dd($reservation);
+        
+        if (is_null($reservation)) {
+            throw new Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now(),
+        ]);
+    }
+
     public function setAuthorIdAttribute($author)
     {
         $this->attributes['author_id'] = (Author::firstOrCreate([
             'name' => $author,
         ]))->id;
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
