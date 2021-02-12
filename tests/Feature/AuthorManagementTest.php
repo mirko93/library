40335ba6a29ce4047ace3525b657099216f141c2
Mirko Skopljak <45762923+mirko93@users.noreply.github.com>
2021-02-12 12:00:18 +0100

    working progress for authors and change route from author to authors

--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -15,15 +15,40 @@ class AuthorManagementTest extends TestCase
     /** @test */
     public function an_author_can_be_created()
     {
-        $this->post('/author', [
-            'name' => 'Author Name',
-            'dob' => '11/13/1993'
-        ]);
+        $this->post('/authors', $this->data());
 
         $author = Author::all();
 
         $this->assertCount(1, $author);
         $this->assertInstanceOf(Carbon::class, $author->first()->dob);
         $this->assertEquals('13/11/1993', $author->first()->dob->format('d/m/Y'));
+    }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+        $response = $this->post('/authors', 
+            array_merge($this->data(), ['name' => '']));
+
+        $response->assertSessionHasErrors('name');
+        
+    }
+
+    /** @test */
+    public function a_dob_is_required()
+    {
+        $response = $this->post('/authors', 
+            array_merge($this->data(), ['dob' => '']));
+
+        $response->assertSessionHasErrors('dob');
+        
+    }
+
+    private function data()
+    {
+        return [
+            'name' => 'Author Name',
+            'dob' => '11/13/1993'
+        ];
     }
 }
